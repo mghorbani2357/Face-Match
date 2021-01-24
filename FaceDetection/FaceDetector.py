85710e5c8bd25041bc227ada0973402bec1bf7d9
ghorbani <m.ghorbani2357@gmail.com>
2021-01-24 19:17:07 +0330

    Import packages.

---
+++ FaceDetection/FaceDetector.py
@@ -0,0 +1,74 @@
+import cv2
+import numpy as np
+from PIL import Image
+
+
+class Detector:
+    image_height = 160
+    image_width = 160
+
+    def __init__(self):
+        #  Load Face Detector
+        deploy = "packages/FaceDetection/deploy.json"
+        model_path = "packages/FaceDetection/res10_300x300_ssd_iter_140000.caffemodel"
+
+        self.detector = cv2.dnn.readNetFromCaffe(deploy, model_path)
+
+    def detect_faces(self, frame):
+        (h, w) = frame.shape[:2]
+
+        # construct a blob from the image
+        image_blob = cv2.dnn.blobFromImage(
+            cv2.resize(frame, (300, 300)), 1.0, (300, 300),
+            (104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+        # apply OpenCV's deep learning-based face detector to localize faces in the input image
+        self.detector.setInput(image_blob)
+        detections = self.detector.forward()
+
+        detected = dict()
+
+        for i in range(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with the prediction
+            confidence = detections[0, 0, i, 2]
+
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            if confidence > 0.5:
+                detected[detected.__len__()] = {
+                    'confidence': confidence,
+                    'box': {
+                        'start_point': (startX, startY), 'end_point': (endX, endY)
+                    },
+                    'face': frame[startY:endY, startX:endX]
+
+                }
+
+        return list(detected.values())
+
+    def align(self, face):
+        return np.array(
+            Image.fromarray(face).resize((self.image_height, self.image_width), Image.BILINEAR)).astype(
+            np.double)
+
+    def align_all_face(self, frame):
+        faces = []
+        # boxes = [face['box'] for face in extracted_face]
+        for face in self.detect_faces(frame):
+            # cropped = frame[box['start_point'][0]:box['start_point'][1], box['end_point'][0]:box['end_point'][1], :]
+            scaled = np.array(
+                Image.fromarray(face).resize((self.image_height, self.image_width), Image.BILINEAR)).astype(
+                np.double)
+            faces.append(scaled)
+        return faces
+
+    def crop_detected_face(self, frame, box):
+        frame = np.array(frame)
+        cropped = frame[box['end_point'][0]:box['end_point'][1], box['start_point'][0]:box['start_point'][1], :]
+
+        # cropped = Image.crop((box['start_point'][1], box['end_point'][1], box['start_point'][0], box['end_point'][0]))
+        scaled = np.array(
+            Image.fromarray(cropped).resize((self.image_height, self.image_width), Image.BILINEAR)).astype(
+            np.double)
+        return scaled
---
+++ FaceVerification/OneShotFaceVerification.py
@@ -0,0 +1,65 @@
+import os
+
+import cv2
+import numpy as np
+from packages.Database.image_dataset import Dataset
+from packages.FaceDetection.FaceDetector import Detector
+
+import packages.FaceVerification.FaceToolKit as ftk
+
+
+class Verifier:
+    verification_threshold = 1.188
+    image_size = 160
+
+    def __init__(self, dataset_path):
+        self.verifier = ftk.Verification()
+        self.verifier.load_model("./models/20180204-160909/")
+        self.verifier.initial_input_output_tensors()
+        self.dataset = Dataset(dataset_path)
+        self.dataset_face_detector = Detector()
+
+    def img_to_encoding(self, aligned_image):
+        return self.verifier.img_to_encoding(aligned_image, self.image_size)
+
+    @staticmethod
+    def distance(emb1, emb2):
+        diff = np.subtract(emb1, emb2)
+        return np.sum(np.square(diff))
+
+    def verify(self, pendent_identity, defined_identity):
+
+        dist = self.distance(pendent_identity, defined_identity)
+
+        if dist < self.verification_threshold:
+            return True, dist
+        else:
+            return False, dist
+
+    def who_is_it(self, face):
+        identity = '404'
+
+        encoded_face = self.img_to_encoding(face)
+
+        min_dist = 1000
+        for image_path in self.dataset.images():
+            image = cv2.imread(image_path)
+
+            detected_face = self.dataset_face_detector.detect_faces(image)[0]
+            detected_face = self.dataset_face_detector.detect_faces(image)[0]
+
+            aligned_face = self.dataset_face_detector.align(detected_face['face'])
+
+            encoded_dataset_image = self.img_to_encoding(aligned_face)
+
+            verified, dist = self.verify(encoded_face, encoded_dataset_image)
+
+            if dist < min_dist:
+                min_dist = dist
+                identity = os.path.basename(image_path)
+                # identity = os.path.splitext(os.path.basename(image_path))[0]
+
+        if min_dist < self.verification_threshold:
+            return identity
+        else:
+            return '404'
